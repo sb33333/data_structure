0b39f856c533e37335ff14e7993c736270727a30
sb33333 <97681301+sb33333@users.noreply.github.com>
2024-07-19 13:19:35 +0900

    Update PathSearchUtilTest.java

--- src/test/java/home/data_structure/path_search/PathSearchUtilTest.java
+++ src/test/java/home/data_structure/path_search/PathSearchUtilTest.java
@@ -1,11 +1,11 @@
 package home.data_structure.path_search;
 
-import java.util.Set;
-import java.util.function.Predicate;
+import java.util.*;
 
 import home.data_structure.graph.*;
 import org.junit.jupiter.api.*;
 
+import home.data_structure.graph.*;
 import home.data_structure.graph.path_search.PathSearchUtil;
 
 public class PathSearchUtilTest {
@@ -64,9 +64,40 @@ public class PathSearchUtilTest {
                     .setAbortCondition(p -> false)
                     .and()
                 .search();
+        result = filterDuplicatePathsByVertexOrder(result);
+        List<Path<String>> sorted = result.stream().sorted(Comparator.comparing((Path<String> p) ->{
+           return p.getVisitedVertices().toString(); 
+        })).collect(Collectors().toList());
+            
         for(Path<String> p : result) {
             System.out.println(":::"+p);
         }
         Assertions.assertFalse(result.isEmpty());
+    }
+
+    /**
+     * remove duplicate paths that visit vertices in the same order.
+     */
+    private <T> Set<Path<T>> filterDuplicatePathsByVertexOrder (Set<Path<T>> result) {
+
+        Set<Path<T>> filtered = new HashSet<>();
+        for (Path<T> p:result) {
+            boolean noEquals = filtered.stream()
+                .map(path -> path.getVisitedVertices())
+                .map(pp -> {
+                    List<Vertex<T>> history = p.getVisitedVertices();
+                    int size = pp.size();
+                    if(size != history.size()) return false;
+                    boolean check = true;
+                    for(int i=0;i<size;i++) {
+                        if(!check) continue;
+                        check = pp.get(i).equals(history.get(i));
+                    }
+                    return check;
+                })
+                .noneMatch(check -> check==true);
+            if(noEquals) filtered.add(p);
+        }
+        return filtered;
     }
 }
